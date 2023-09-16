08ac84e64801ca82e882ee987e0170e6785090f4
Gavin Castaneda <gcastaneda456@gmail.com>
2023-09-16 14:19:12 -0600

    begin setting up canvas with points

--- montecarlo/src/main.rs
+++ montecarlo/src/main.rs
@@ -1,9 +1,19 @@
 use speedy2d::color::Color;
 use speedy2d::{Graphics2D, Window};
 use speedy2d::window::{WindowHandler, WindowHelper};
+use montecarlo::*;
 
 fn main() {
-    let window = Window::new_centered("Hello, world!", (640, 480)).unwrap();
+    // Config variables
+    let width = 1920;
+    let height = 1080;
+
+    // Setup points
+    let canvas = Canvas::new(width, height);
+    //TODO: Setup points here
+    
+    // Setup Window
+    let window = Window::new_centered("Monte Carlo", (width, height)).unwrap();
     window.run_loop(MyWindowHandler{});
 }
 
@@ -12,7 +22,8 @@ struct MyWindowHandler {}
 impl WindowHandler for MyWindowHandler {
     fn on_draw(&mut self, helper: &mut WindowHelper, graphics: &mut Graphics2D) {
         graphics.clear_screen(Color::from_rgb(0.8, 0.9, 1.0));
-        graphics.draw_circle((100.0, 100.0), 70.0, Color::BLUE);
+        // graphics.draw_circle((100.0, 100.0), 70.0, Color::BLUE);
+        // graphics.draw_circle((100.0, 100.0), 2.0, Color::BLACK);
 
         helper.request_redraw();
     }
